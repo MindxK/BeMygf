a8f77e924337f2b47f5fddd0b1e8a04ff0be2750
sea <muesoel106@gmail.com>
2026-04-20 13:44:19 +0700

    testbtnno1

--- js/script.js
+++ js/script.js
@@ -4,15 +4,23 @@ const msgno = [
     "1",
     "2",
     "3",
-    "4"
+    "4",
+    "5",
+    "6"
 ];
 
 function cilckno() {
     const msg = document.getElementById("msno")
+    const bear = document.getElementById("bear")
+    const btnno = document.getElementById("btnno")
     if (Nocount < msgno.length - 1) {
         Nocount = Nocount + 1 } 
     msg.textContent = msgno[Nocount]
-    msg.style.display = "block"
+    msg.style.display = 'block'
+
+    btnno.style.position = 'relative'
+    btnno.style.left = (Math.random() * 50) + 'px'
+    btnno.style.top = (Math.random() * 50) + 'px'
 }
 
 function cilckyes() {
